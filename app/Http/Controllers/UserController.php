094bdcd243119b9db03e4b56b794cb5cc4564848
emmanuel patatag <emmanuel.patatag@carsu.edu.ph>
2023-10-26 23:36:32 +0800

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use App\Http\Requests\UserRequest;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return User::all();
+    }
+
+    
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(UserRequest $request)
+    {
+        $validated = $request->validated();
+        $validated['password'] = Hash::make($validated['password']);
+        $user = User::create($validated);
+        return $user;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return User::findOrfail($id);
+    }
+
+    public function destroy(string $id)
+    {
+        $user = User::findOrfail($id);
+ 
+        $user->delete();
+        return $user;
+    }
+
+}
+
